5d74f6c7c09ceb4b68bd0265e6c641192080d994
Santo Cariotti <santo@dcariotti.me>
2022-09-18 14:24:32 +0200

    Add model loading component

---
+++ components/ModelLoading.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  .grid.grid-cols-4.gap-4
+    .model.shadow-xlg.rounded-lg.bg-white.p-4.w-full.h-80.animate-pulse
+      .image.bg-green-100.rounded-xl.w-full.h-48
+      .space-y-3.mt-5
+        .grid.grid-cols-3.gap-4
+          .h-4.bg-green-100.rounded-xl.col-span-2
+          .h-4.bg-green-100.rounded-xl.col-span-1
+        .h-4.bg-green-100.rounded-xl
+
+</template>
+
+<script>
+export default {
+  name: "ModelLoading",
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,29 @@
 <template lang="pug">
   .mx-auto.max-w-7xl.py-6(class="sm:px-6 lg:px-8")
+    model-loading(v-if="isLoading")
+
 </template>
 
 <script>
+import ModelLoading from "@/components/ModelLoading.vue";
+
+import { mapGetters } from "vuex";
+
 export default {
   name: "IndexPage",
   layout: "default",
   head: {
     title: "Verden - Social for 3D artists",
+  },
+  components: {
+    "model-loading": ModelLoading,
+  },
+  computed: {
+    ...mapGetters(["isLoading"]),
+    ...mapGetters("auth", ["isLogged"]),
+  },
+  created() {
+    this.$store.dispatch("models/getModels");
   },
 };
 </script>
