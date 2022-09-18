0cd059d6318276376236be13438133a285478f3c
Santo Cariotti <santo@dcariotti.me>
2022-09-18 19:16:12 +0200

    Fix avatar without image

--- components/UserAvatar.vue
+++ components/UserAvatar.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   div(v-if="data")
     img.h-8.w-8.rounded-full(v-if="data.avatar" :src="baseAPI+''+data.avatar" alt="Avatar")
-    span.h-8.w-8.rounded-full.bg-white.pt-1(v-else) {{ data.username[0] }}
+    span.h-8.w-8.rounded-full.bg-gray-100.text-center.pt-1.block(v-else) {{ data.username[0] }}
 </template>
 
 <script>
