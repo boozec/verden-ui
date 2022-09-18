9382162c78a2991804885c8a7e36b599f45d0038
Santo Cariotti <santo@dcariotti.me>
2022-09-18 18:46:42 +0200

    Component for user avatar

---
+++ components/UserAvatar.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  div(v-if="data")
+    img.h-8.w-8.rounded-full(v-if="data.avatar" :src="baseAPI+''+data.avatar" alt="Avatar")
+    span.h-8.w-8.rounded-full.bg-white.pt-1(v-else) {{ data.username[0] }}
+</template>
+
+<script>
+export default {
+  name: "UserAvatar",
+  props: ["data"],
+  data() {
+    return {
+      baseAPI: "",
+    };
+  },
+  created() {
+    this.baseAPI = this.$config.api;
+  },
+};
+</script>
