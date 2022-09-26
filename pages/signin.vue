b48629c9c61a8fc8a950195d4ec4281cfdfb74da
Santo Cariotti <santo@dcariotti.me>
2022-09-26 15:20:49 +0200

    Drop "remember me" checkbox

--- pages/signin.vue
+++ pages/signin.vue
@@ -15,10 +15,6 @@
             div
               label.sr-only(for="password") Password
               input#password.relative.block.w-full.appearance-none.rounded-none.rounded-b-md.border.border-gray-300.px-3.py-2.text-gray-900.placeholder-gray-500(name="password" type="password" autocomplete="current-password" required class="focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Password" v-model="form.password")
-          .flex.items-center.justify-between
-            .flex.items-center
-              input#remember-me.h-4.w-4.rounded.border-gray-300.text-indigo-600(name="remember-me" type="checkbox" class="focus:ring-indigo-500")
-              label.ml-2.block.text-sm.text-gray-900(for="remember-me") Remember me
           div
             button.group.relative.flex.w-full.justify-center.rounded-md.border.border-transparent.bg-indigo-600.py-2.px-4.text-sm.font-medium.text-white(type="submit" class="hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" @click="save")
               span.absolute.inset-y-0.left-0.flex.items-center.pl-3
