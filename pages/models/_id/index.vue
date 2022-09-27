ef309b83108847cbdf5565d8a459264926266264
Santo Cariotti <santo@dcariotti.me>
2022-09-27 21:38:51 +0200

    Fix text white only for dark

--- pages/models/_id/index.vue
+++ pages/models/_id/index.vue
@@ -128,37 +128,37 @@
 
     section.mt-5
       .grid.grid-cols-2.gap-5.mb-5(class="md:grid-cols-6")
-        .card.shadow-sm.rounded-lg.bg-white.p-4.w-full(class="dark:bg-gray-600 text-white")
+        .card.shadow-sm.rounded-lg.bg-white.p-4.w-full(class="dark:bg-gray-600 dark:text-white")
           h3.font-bold Created: 
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
           </svg>
           p(:title="model.created|moment('DD/MM/YYYY HH:mm')") {{ model.created|moment("DD/MM/YYYY") }}
-        .card.shadow-sm.rounded-lg.bg-white.p-4.w-full(class="dark:bg-gray-600 text-white")
+        .card.shadow-sm.rounded-lg.bg-white.p-4.w-full(class="dark:bg-gray-600 dark:text-white")
           h3.font-bold Duration: 
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
             <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM12.75 6a.75.75 0 00-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 000-1.5h-3.75V6z" clip-rule="evenodd" />
           </svg>
           p {{ model.duration }}m
-        .card.shadow-sm.rounded-lg.bg-white.p-4.w-full(class="dark:bg-gray-600 text-white")
+        .card.shadow-sm.rounded-lg.bg-white.p-4.w-full(class="dark:bg-gray-600 dark:text-white")
           h3.font-bold Height: 
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25" />
           </svg>
           p {{ model.height }}mm
-        .card.shadow-sm.rounded-lg.bg-white.p-4.w-full(class="dark:bg-gray-600 text-white")
+        .card.shadow-sm.rounded-lg.bg-white.p-4.w-full(class="dark:bg-gray-600 dark:text-white")
           h3.font-bold Weight: 
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v17.25m0 0c-1.472 0-2.882.265-4.185.75M12 20.25c1.472 0 2.882.265 4.185.75M18.75 4.97A48.416 48.416 0 0012 4.5c-2.291 0-4.545.16-6.75.47m13.5 0c1.01.143 2.01.317 3 .52m-3-.52l2.62 10.726c.122.499-.106 1.028-.589 1.202a5.988 5.988 0 01-2.031.352 5.988 5.988 0 01-2.031-.352c-.483-.174-.711-.703-.59-1.202L18.75 4.971zm-16.5.52c.99-.203 1.99-.377 3-.52m0 0l2.62 10.726c.122.499-.106 1.028-.589 1.202a5.989 5.989 0 01-2.031.352 5.989 5.989 0 01-2.031-.352c-.483-.174-.711-.703-.59-1.202L5.25 4.971z" />
           </svg>
           p {{ model.weight }}g
-        .card.shadow-sm.rounded-lg.bg-white.p-4.w-full(class="dark:bg-gray-600 text-white" v-if="model.printer")
+        .card.shadow-sm.rounded-lg.bg-white.p-4.w-full(class="dark:bg-gray-600 dark:text-white" v-if="model.printer")
           h3.font-bold Printer: 
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M6.72 13.829c-.24.03-.48.062-.72.096m.72-.096a42.415 42.415 0 0110.56 0m-10.56 0L6.34 18m10.94-4.171c.24.03.48.062.72.096m-.72-.096L17.66 18m0 0l.229 2.523a1.125 1.125 0 01-1.12 1.227H7.231c-.662 0-1.18-.568-1.12-1.227L6.34 18m11.318 0h1.091A2.25 2.25 0 0021 15.75V9.456c0-1.081-.768-2.015-1.837-2.175a48.055 48.055 0 00-1.913-.247M6.34 18H5.25A2.25 2.25 0 013 15.75V9.456c0-1.081.768-2.015 1.837-2.175a48.041 48.041 0 011.913-.247m10.5 0a48.536 48.536 0 00-10.5 0m10.5 0V3.375c0-.621-.504-1.125-1.125-1.125h-8.25c-.621 0-1.125.504-1.125 1.125v3.659M18 10.5h.008v.008H18V10.5zm-3 0h.008v.008H15V10.5z" />
           </svg>
           p {{ model.printer }}
-        .card.shadow-sm.rounded-lg.bg-white.p-4.w-full(class="dark:bg-gray-600 text-white" v-if="model.material")
+        .card.shadow-sm.rounded-lg.bg-white.p-4.w-full(class="dark:bg-gray-600 dark:text-white" v-if="model.material")
           h3.font-bold Material: 
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 3.104v5.714a2.25 2.25 0 01-.659 1.591L5 14.5M9.75 3.104c-.251.023-.501.05-.75.082m.75-.082a24.301 24.301 0 014.5 0m0 0v5.714c0 .597.237 1.17.659 1.591L19.8 15.3M14.25 3.104c.251.023.501.05.75.082M19.8 15.3l-1.57.393A9.065 9.065 0 0112 15a9.065 9.065 0 00-6.23-.693L5 14.5m14.8.8l1.402 1.402c1.232 1.232.65 3.318-1.067 3.611A48.309 48.309 0 0112 21c-2.773 0-5.491-.235-8.135-.687-1.718-.293-2.3-2.379-1.067-3.61L5 14.5" />
