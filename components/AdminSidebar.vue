cb378cd2b8d2ebbd9b88b5f6dd669569b346af16
Santo Cariotti <santo@dcariotti.me>
2022-10-13 22:26:49 +0200

    Fix links

--- components/AdminSidebar.vue
+++ components/AdminSidebar.vue
@@ -9,13 +9,13 @@
     li
       a.block.border-l.pl-4.border-transparent.text-gray-700(
         :class="{'hover:border-gray-400 dark:hover:border-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-300': true, 'font-semibold hover:text-green-800 text-green-700 dark:text-green-400 dark:hover:text-green-500 border-green-500': page == 'admin-models'}"
-        href="/models"
+        href="/admin/models"
       )
         | Models
     li
       a.block.border-l.pl-4.border-transparent.text-gray-700(
         :class="{'hover:border-gray-400 dark:hover:border-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-300': true, 'font-semibold hover:text-green-800 text-green-700 dark:text-green-400 dark:hover:text-green-500 border-green-500': page == 'admin-reports'}"
-        href="/reports"
+        href="/admin/reports"
       )
         | Reports
 </template>
